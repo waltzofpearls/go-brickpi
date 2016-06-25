94358095a4f6f39996358ad7bd6ea81e57aaa6f1
Rollie Ma <rollie.ma@gmail.com>
2016-06-25 23:36:27 +0000

    Completed SetupSensors() method

--- brickpi.go
+++ brickpi.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"time"
 
 	"github.com/waltzofpearls/serial"
@@ -106,9 +105,16 @@ const (
 	IndexBlank = 3
 )
 
+var (
+	Array         [256]int
+	BytesReceived int
+	BitOffset     int
+	Retried       int
+)
+
 type BrickPi struct {
 	Serial     *serial.Port
-	Address    [2]uint8
+	Address    [2]int
 	MotorSpeed [4]uint8
 
 	MotorEnable [4]uint8
@@ -118,21 +124,21 @@ type BrickPi struct {
 
 	Sensor         [4]uint8
 	SensorArray    [4][4]uint8
-	SensorType     [4]uint8
-	SensorSettings [4][8]uint8
+	SensorType     [4]int
+	SensorSettings [4][8]int
 
-	SensorI2CDevices [4]uint8
-	SensorI2CSpeed   [4]uint8
-	SensorI2CAddr    [4][8]uint8
-	SensorI2CWrite   [4][8]uint8
-	SensorI2CRead    [4][8]uint8
-	SensorI2COut     [4][8][16]uint8
-	SensorI2CIn      [4][8][16]uint8
+	SensorI2cDevices [4]int
+	SensorI2cSpeed   [4]int
+	SensorI2cAddr    [4][8]int
+	SensorI2cWrite   [4][8]int
+	SensorI2cRead    [4][8]int
+	SensorI2cOut     [4][8][16]int
+	SensorI2cIn      [4][8][16]int
 	Timeout          int
 }
 
 func New() *BrickPi {
-	return &BrickPi{Address: [2]uint8{1, 2}}
+	return &BrickPi{Address: [2]int{1, 2}}
 }
 
 func (bp *BrickPi) Setup() error {
@@ -148,12 +154,81 @@ func (bp *BrickPi) Setup() error {
 	return nil
 }
 
-func (bp *BrickPi) SetupSensors() error {
-	return nil
+func (bp *BrickPi) SetupSensors() int {
+	for i := 0; i < 2; i++ {
+		Array = [256]int{}
+		BitOffset = 0
+		Array[ByteMsgType] = MsgTypeSensorType
+		Array[ByteSensor1Type] = bp.SensorType[Port1+i*2]
+		Array[ByteSensor2Type] = bp.SensorType[Port2+i*2]
+		for ii := 0; ii < 2; ii++ {
+			port := i*2 + ii
+			if Array[ByteSensor1Type+ii] == TypeSensorUltrasonicCont {
+				Array[ByteSensor1Type+ii] = TypeSensorI2c
+				bp.SensorI2cSpeed[port] = UsI2cSpeed
+				bp.SensorI2cDevices[port] = 1
+				bp.SensorSettings[port][UsI2cIdx] = BitI2cMid | BitI2cSame
+				bp.SensorI2cAddr[port][UsI2cIdx] = LegoUsI2cAddr
+				bp.SensorI2cWrite[port][UsI2cIdx] = 1
+				bp.SensorI2cRead[port][UsI2cIdx] = 1
+				bp.SensorI2cOut[port][UsI2cIdx][0] = LegoUsI2cDataReg
+			}
+			if Array[ByteSensor1Type+ii] == TypeSensorI2c || Array[ByteSensor1Type+ii] == TypeSensorI2c9v {
+				bp.AddBits(3, 0, 8, bp.SensorI2cSpeed[port])
+
+				if bp.SensorI2cDevices[port] > 8 {
+					bp.SensorI2cDevices[port] = 8
+				}
+
+				if bp.SensorI2cDevices[port] == 0 {
+					bp.SensorI2cDevices[port] = 1
+				}
+
+				bp.AddBits(3, 0, 3, (bp.SensorI2cDevices[port] - 1))
+
+				for device := 0; device < bp.SensorI2cDevices[port]; device++ {
+					bp.AddBits(3, 0, 7, (bp.SensorI2cAddr[port][device] >> 1))
+					bp.AddBits(3, 0, 2, bp.SensorSettings[port][device])
+					if bp.SensorSettings[port][device]&BitI2cSame != 0 {
+						bp.AddBits(3, 0, 4, bp.SensorI2cWrite[port][device])
+						bp.AddBits(3, 0, 4, bp.SensorI2cRead[port][device])
+
+						for outByte := 0; outByte < bp.SensorI2cWrite[port][device]; outByte++ {
+							bp.AddBits(3, 0, 8, bp.SensorI2cOut[port][device][outByte])
+						}
+					}
+				}
+			}
+		}
+		txBytes := (((BitOffset + 7) / 8) + 3) // eq to UartTxBytes
+		bp.BrickPiTx(bp.Address[i], txBytes, Array)
+		res, BytesReceived, InArray := bp.BrickPiRx(5) // Timeout set to 5 seconds to setup EV3 sensors successfully
+		if res != 0 {
+			return -1
+		}
+		for i = 0; i < len(InArray); i++ {
+			Array[i] = InArray[i]
+		}
+		if !(BytesReceived == 1 && Array[ByteMsgType] == MsgTypeSensorType) {
+			return -1
+		}
+	}
+	return 0
+}
+
+func (bp *BrickPi) AddBits(byteOffset, bitOffset, bits, value int) {
+}
+
+func (bp *BrickPi) BrickPiTx(dest, ByteCount int, OutArray [256]int) {
+}
+
+func (bp *BrickPi) BrickPiRx(timeout int) (int, int, [256]int) {
+	var InArray [256]int
+	return 0, 0, InArray
 }
 
 func main() {
 	bp := New()
-	fmt.Printf("%v\n", TypeSensorLightOn)
-	fmt.Printf("%v\n", bp.Setup())
+	bp.Setup()
+	bp.SetupSensors()
 }
